562a15beeb7ac5418e79b95a263f795d859699d8
littlesorry <chaumet.g@gmail.com>
2014-11-20 10:25:03 +0800

    select panel prototype

--- public/app/city.js
+++ public/app/city.js
@@ -1,8 +1,18 @@
 (function () {
 	var ns = Q.use("bag");
 
-	var city = ns.city = {};
+	var template = "<div id='states' class='panel'></div>";
+	var listItem = "<li class='item'></li>";
 
-	
+
+	var city = ns.city = {
+		selectState: function() {
+			$("<div id='states' class='panel'/>")
+				.appendTo('body');
+		},
+		selectCity: function() {
+
+		}
+	};
 
 })();
