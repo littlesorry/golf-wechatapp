c755dbe00a470d043bff7a57e46a65dfdfcf83d2
littlesorry <chaumet.g@gmail.com>
2014-11-21 11:02:37 +0800

    refine select panel with handlebars

--- public/app/city.js
+++ public/app/city.js
@@ -1,48 +1,61 @@
 (function () {
 	var ns = Q.use("bag");
 
-	var states = [
-		"北京",
-		"上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",	
-		"重庆"
-	];
+	var states = {
+		items: [
+			{name: "北京"},
+			{name: "上海"},
+			{name: "天津"},
+			{name: "重庆"},
+			{name: "北京"},
+			{name: "上海"},
+			{name: "天津"},
+			{name: "重庆"},
+			{name: "北京"},
+			{name: "上海"},
+			{name: "天津"},
+			{name: "重庆"},
+			{name: "北京"},
+			{name: "上海"},
+			{name: "天津"},
+			{name: "重庆"},
+		]
+	};
 
-	var cities = [
-		"1北京",
-		"上海",
-		"天津",
-		"2上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",
-		"上海",
-		"天津",	
-		"重庆"
-	];
+	var cities = {
+		items: [
+			{name: "北京1"},
+			{name: "上海1"},
+			{name: "天津"},
+			{name: "重庆"},
+			{name: "北京"},
+			{name: "上海"},
+			{name: "天津"},
+			{name: "重庆"},
+			{name: "北京"},
+			{name: "上海"},
+			{name: "天津"},
+			{name: "重庆"},
+			{name: "北京"},
+			{name: "上海"},
+			{name: "天津"},
+			{name: "重庆"},
+		]
+	};
 
 	var events = Q.supportTouch ? ["touchstart", "touchmove", "touchend"] : ["mousedown", "mousemove", "mouseup"];
 
+	var panelTpl = "<div id='panel' class='panel animated zoomIn'><ul>"
+					+ "{{#each items}}"
+					+ "<li><a>"
+					+ "{{name}}"
+					+ "</a></li>"
+					+ "{{/each}}"
+					+ "</ul></div>";
+	var template = Handlebars.compile(panelTpl);
+
 	var displayPanel = function(items, callback) {
-		var panel = $("<div id='panel' class='panel animated zoomIn'><ul></ul></div>")
+		var panel = $(template(items))
 			.appendTo('body')
 			.on(events[2], "a", function() {
 				panel.addClass("zoomOut");
@@ -51,11 +64,6 @@
 				});
 				callback && callback($(this).text());
 			});
-
-		for(var i = 0; i < items.length; i++) {
-			$("ul", panel)
-				.append("<li><a>" + items[i] + "</a></li>");
-		}
 	};
 
 	var city = ns.city = {
