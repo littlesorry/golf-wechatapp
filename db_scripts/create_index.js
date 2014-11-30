7a99f671f7696b3018698e5f8302b0cc046366ff
littlesorry <chaumet.g@gmail.com>
2014-11-30 21:47:11 +0800

    correct syntax

--- db_scripts/create_index.js
+++ db_scripts/create_index.js
@@ -1,2 +1,2 @@
-db.coupons.ensureIndex( { "openId": 1, "type", 1 }, { unique: true, sparse: true } );
+db.coupons.ensureIndex( { "openId": 1, "type": 1 }, { unique: true, sparse: true } );
 db.coupons.ensureIndex( { "type": 1, "statues": 1 });
