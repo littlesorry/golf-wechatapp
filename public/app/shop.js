41a1c930bff4c8cf0049de5a1f77634e9098ae3c
littlesorry <chaumet.g@gmail.com>
2014-12-13 16:15:21 +0800

    update font size

--- public/app/shop.js
+++ public/app/shop.js
@@ -43,7 +43,7 @@
 			shopText.lineSpacing = 29; 
 			shopText.color = "#fff";
 			shopText.text = data[i].address;
-			shopText.font = "26px 黑体";
+			shopText.font = "24px 黑体";
 			initLocationPosition(shopText, i);
 
 			ns.game.stage.addChild(shopText);
